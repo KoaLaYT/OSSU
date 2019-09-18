7ef086751983cd1149c397ee50c48c1d3b4c5707
KoaLaYT <hytohyeah@outlook.com>
2019-09-18 22:42:06 +0800

    finish chapter 9 : Observable Pattern

--- Core-CS/1-Core-Programming/4-Software-Construction-Object-Oriented-Design/9-Design-Patterns/long-form-problem-starters/ConsoleArcade/BingoStarter/src/model/Game.java
+++ Core-CS/1-Core-Programming/4-Software-Construction-Object-Oriented-Design/9-Design-Patterns/long-form-problem-starters/ConsoleArcade/BingoStarter/src/model/Game.java
@@ -1,21 +1,22 @@
 package model;
 
+import model.observer_pattern.Observer;
+import model.observer_pattern.Subject;
 import model.random.BingoNumber;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Game {
+public class Game extends Subject {
 
     public static final int CARD_SIZE = 25;
     public static final int SIDE_LENGTH = (int) Math.sqrt(CARD_SIZE);
 
     private BingoNumber currentCall;
-    private List<PlayerCard> cards;
     private boolean gameOver;
 
     public Game() {
-        cards = new ArrayList<>();
+        super();
         callNext();
     }
 
@@ -30,29 +31,23 @@ public class Game {
 
     public List<PlayerCard> getCards() {
         List<PlayerCard> playerCards = new ArrayList<>();
-        for (PlayerCard o : cards) { //NOTE: refactor this line ONLY.
+        for (Observer o : getObservers()) { //NOTE: refactor this line ONLY.
             if (o.getClass().getSimpleName().equals("PlayerCard"))
                 playerCards.add((PlayerCard) o);
         }
         return playerCards;
     }
 
-    //TODO: refactor this method
     //EFFECTS: generates the next bingo call and notifies observers
     public void callNext() {
         currentCall = new BingoNumber();
-    }
-
-    //TODO: refactor this method
-    //MODIFIES: this
-    //EFFECTS: adds observer to list of observers
-    public void addCard(PlayerCard pc) {
-        cards.add(pc);
+        notifyObservers(currentCall);
+        refreshGameOver();
     }
 
     //EFFECTS: sets game over to true if one of the players has bingo
     public void refreshGameOver(){
-        for (PlayerCard pc : cards) {
+        for (Observer pc : getObservers()) {
             PlayerCard p = (PlayerCard) pc;
             if (p.hasBingo()) {
                 gameOver = true;
