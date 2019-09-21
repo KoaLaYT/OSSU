406ce0d06dc3077832c8a1110aef9ee320c33940
KoaLaYT <hytohyeah@outlook.com>
2019-09-21 14:21:49 +0800

    finished final project

---
+++ Core-CS/1-Core-Programming/4-Software-Construction-Object-Oriented-Design/10-Final-Project/final-project-starter/TwitterMapperStarter/src/filters/OrFilter.java
@@ -0,0 +1,37 @@
+package filters;
+
+import twitter4j.Status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrFilter implements Filter {
+    private final Filter term1;
+    private final Filter term2;
+
+    public OrFilter(Filter t1, Filter t2) {
+        term1 = t1;
+        term2 = t2;
+    }
+
+    @Override
+    public boolean matches(Status s) {
+        return term1.matches(s) || term2.matches(s);
+    }
+
+    @Override
+    public List<String> terms() {
+        List<String> res = new ArrayList<>();
+        for (String t : term1.terms()) {
+            res.add(t);
+        }
+        for (String t : term2.terms()) {
+            res.add(t);
+        }
+        return res;
+    }
+
+    public String toString() {
+        return "(" + term1.toString() + " or " + term2.toString() + ")";
+    }
+}
